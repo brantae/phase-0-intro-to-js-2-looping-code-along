30ca18186fdb49ca011197b6b75358dee7f70ee2
Anslie <brantae@gmail.com>
2022-07-28 18:09:38 -0500

    commit tree 2823fce3340a5c81452a83e533ad0e59e0e6004c

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+ const names = ["Guadalupe", "Ollie", "Aki"];
+ const event = "surprise";
+
+function writeCards(names, event) {
+    let newNames = [];
+    
+    for (let i = 0; i < names.length; i++) {
+        newNames.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;
+  }
+
+  return newNames;
+}
+
+// writeCards(newNames);
+
+function countDown() {
+    let i = 10;
+    while (i >= 0) {
+        console.log(i--);
+    }
+    debugger;
+}
+countDown();
